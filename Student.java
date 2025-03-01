6fa3b4615ac2ca472295035a55302706edb0889c
Kashvi Singh <kashvi1401@gmail.com>
2025-03-01 14:23:51 +0530

    Create Parameterized constructor

--- Student.java
+++ Student.java
@@ -1,5 +1,17 @@
 // Student.java
 
 class Student {
+ private String name;
+ private long prn;
+ private String branch;
+ private String batch;
+ private double cgpa;
 
+ public Student(String name, long prn, String branch, String batch, double cgpa) {
+  this.name = name;
+  this.prn = prn;
+  this.branch = branch;
+  this.batch = batch;
+  this.cgpa = cgpa;
+ }
 }
